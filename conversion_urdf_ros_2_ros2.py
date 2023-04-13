f6055a059e5acfae7deefe07d8c0a9ba089b29f1
Marc Bestmann <marc.bestmann@dlr.de>
2023-04-13 11:07:25 +0200

    automatically generate complete package; fill out package xml; add rviz conf; fix warnings

--- conversion_urdf_ros_2_ros2.py
+++ conversion_urdf_ros_2_ros2.py
@@ -4,17 +4,28 @@ import sys
 
 # Configuration variable
 
-# This variable is the path to the solidworks output folder of urdf files
-source_dir = '/home/sxm/Project/ros/test_urdf/'
+# This variable is the path to the solidworks output folder containing the urdf files
+source_dir = '../solidworks_export/'
 
-# This variable is the package path of ros2
-target_dir = '/home/sxm/Project/ros/test_urdf_tool_ws/src/test_urdf_tool/'
+# This variable is the name for the ROS 2 package
+package_name = "system_x_description"
 
-# This variable is the package-name of ros2
-package_name = "test_urdf_tool"
+# This variable is the path for the ROS 2 package
+package_directory = '../'
+target_dir = package_directory + package_name + '/'
 
-# This variable is the solidworks output folder name
-output_folder_name = "test_urdf"
+# This variable is the name of the URDF file that SolidWorks outputs
+source_urdf_file_name = "7mm_Concept_Group.SLDASM"
+
+# This variable defines the name of the target URDF in the ROS 2 package
+target_urdf_file_name = "robot"
+
+# Maintainer, description, license and version that will be added to the package xml and setup.py
+maintainer_name = "Marc Bestmann"
+maintainer_mail = "marc.bestmann@dlr.de"
+description = "Description package for the System-X robot"
+license_type = "TODO"
+version_number = "0.0.1"
 
 
 def run_command_dir(command_dir, command):
@@ -33,45 +44,82 @@ def replace_str(file, old_str, new_str):
 
 
 if __name__ == '__main__':
-    # Create folders
-    run_command_dir(target_dir, "mkdir launch meshes meshes/collision meshes/visual urdf")
+    # Check if this is a new package or if we might override something
+    if os.path.exists(target_dir) and (os.listdir(target_dir) != 0):
+        sys.stdout.write(
+            f"Target directory {target_dir} is not empty. Previous files might be deleted to create the ROS2 package. Continue? [Y/n]")
+        choice = input().lower()
+        if choice not in ["y", ""]:
+            exit()
+    if not os.path.exists(target_dir):
+        # create the directory of the package
+        run_command_dir(package_directory, f"mkdir {package_name}")
+    # Remove old folders to ensure that we don't keep old files from previous version (i.e. meshes with different names)
+    folders = ["launch", "meshes", "meshes/collision",
+               "meshes/visual", "urdf", "rviz", "resource"]
+    for folder in folders:
+        if os.path.exists(f"{target_dir}/{folder}"):
+            run_command_dir(target_dir, f"rm -r {folder}")
+    # Create new empty folders
+    for folder in folders:
+        run_command_dir(target_dir, f"mkdir {folder}")
 
     # Copy files
     # Copy stl files
-    run_command_dir(source_dir, "cp -r ./meshes/* " + target_dir + "meshes/visual")
-    run_command_dir(source_dir, "cp -r ./meshes/* " + target_dir + "meshes/collision")
+    run_command_dir(source_dir, "cp -r ./meshes/* " +
+                    target_dir + "meshes/visual")
+    run_command_dir(source_dir, "cp -r ./meshes/* " +
+                    target_dir + "meshes/collision")
     # Copy urdf files
-    run_command_dir(source_dir, "cp  ./urdf/" + output_folder_name + ".urdf " + target_dir + "urdf/")
+    run_command_dir(source_dir, "cp  ./urdf/" +
+                    source_urdf_file_name + ".urdf " + target_dir + "urdf/")
+    # rename URDF
+    run_command_dir(target_dir, "mv ./urdf/" +
+                    source_urdf_file_name + ".urdf " + target_dir + "urdf/" + target_urdf_file_name + ".urdf")
+    # create empty file to install marker in the package index
+    run_command_dir(target_dir, f"touch ./resource/{package_name}")
 
-    # replace files
+    # replace file
     os.system("cp -f ./replace_files/setup.py " + target_dir)
+    os.system("cp -f ./replace_files/setup.cfg " + target_dir)
     os.system("cp -f ./replace_files/package.xml " + target_dir)
     os.system("cp -f ./replace_files/launch.py " + target_dir + "launch")
+    os.system("cp -f ./replace_files/default.rviz " + target_dir + "rviz")
 
     # Change file content
     # launch.py
-    replace_str(target_dir + "launch/launch.py", "lesson_urdf", package_name)
-    replace_str(target_dir + "launch/launch.py", "planar_3dof.urdf", output_folder_name + ".urdf")
+    replace_str(target_dir + "launch/launch.py", "PACKAGE_NAME", package_name)
+    replace_str(target_dir + "launch/launch.py",
+                "URDF_NAME.urdf", target_urdf_file_name + ".urdf")
     # setup.py
-    replace_str(target_dir + "setup.py", "lesson_urdf", package_name)
+    replace_str(target_dir + "setup.py", "PACKAGE_NAME", package_name)
+    replace_str(target_dir + "setup.cfg", "PACKAGE_NAME", package_name)
     # package.xml
-    replace_str(target_dir + "package.xml", "lesson_urdf", package_name)
+    replace_str(target_dir + "package.xml", "PACKAGE_NAME", package_name)
+    replace_str(target_dir + "package.xml", "MAINTAINER_NAME", maintainer_name)
+    replace_str(target_dir + "package.xml", "MAINTAINER_MAIL", maintainer_mail)
+    replace_str(target_dir + "package.xml", "LICENSE_TYPE", license_type)
+    replace_str(target_dir + "package.xml", "VERSION_NUMBER", version_number)
+    replace_str(target_dir + "package.xml", "DESCRIPTION_TEXT", description)
     # urdf files
-    replace_str(target_dir + "urdf/" + output_folder_name + ".urdf", output_folder_name + "/meshes",
+    replace_str(target_dir + "urdf/" + target_urdf_file_name + ".urdf", source_urdf_file_name + "/meshes",
                 package_name + "/meshes/visual")
+    replace_str(target_dir + "urdf/" + target_urdf_file_name + ".urdf", source_urdf_file_name,
+                target_urdf_file_name)
 
     # Insert base_footprint
-    keyword = "name=\"" + output_folder_name + "\">"
+    keyword = "name=\"" + target_urdf_file_name + "\">"
     str = ""
     with open("./replace_files/insert_content.txt", "r", encoding="utf-8") as f:
         str = f.read()
-
-    file = open(target_dir + "/urdf/" + output_folder_name + ".urdf", 'r')
+    file = open(target_dir + "/urdf/" + target_urdf_file_name + ".urdf", 'r')
     content = file.read()
     post = content.find(keyword)
     if post != -1:
-        content = content[:post + len(keyword)] + "\n" + str + content[post + len(keyword):]
-        file = open(target_dir + "/urdf/" + output_folder_name + ".urdf", "w")
+        content = content[:post + len(keyword)] + \
+            "\n" + str + content[post + len(keyword):]
+        file = open(target_dir + "/urdf/" +
+                    target_urdf_file_name + ".urdf", "w")
         file.write(content)
     file.close()
 
--- replace_files/setup.py
+++ replace_files/setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 from glob import glob
 
-package_name = 'lesson_urdf'
+package_name = 'PACKAGE_NAME'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, glob('launch/*.py')),
-  	('share/' + package_name+'/urdf/', glob('urdf/*')),
-  	('share/' + package_name+'/rviz/', glob('rviz/*')),
-  	('share/' + package_name+'/meshes/collision/', glob('meshes/collision/*')),
-  	('share/' + package_name+'/meshes/visual/', glob('meshes/visual/*')),
+        ('share/' + package_name+'/urdf/', glob('urdf/*')),
+        ('share/' + package_name+'/rviz/', glob('rviz/*')),
+        ('share/' + package_name+'/meshes/collision/', glob('meshes/collision/*')),
+        ('share/' + package_name+'/meshes/visual/', glob('meshes/visual/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +22,6 @@ setup(
     maintainer_email='TODO:',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
